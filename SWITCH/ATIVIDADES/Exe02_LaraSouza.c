acaaede0106cf42723399267cd5a241163c8cda6
Lara Gorito Barbosa de Souza <laragoritob@gmail.com>
2025-09-26 13:54:00 -0300

    Atividade Salário SWITCH CASE

--- SWITCH/ATIVIDADES/Exe02_LaraSouza.c
+++ SWITCH/ATIVIDADES/Exe02_LaraSouza.c
@@ -26,6 +26,7 @@ int main () {
 				
 				printf("Valor do Imposto: R$", imposto);
 				printf("Salário após o imposto: R$", salarioImposto);
+				
 			else if (salario >= 500 && salario <= 850)
 				imposto = 10/100;
 				salario = salario * imposto;
@@ -33,11 +34,44 @@ int main () {
 				
 				printf("Valor do Imposto: R$", imposto);
 				printf("Salário após o imposto: R$", salarioImposto);
+				
+			else
+				imposto = 15/100;
+				salario = salario * imposto;
+				salarioImposto = salario - imposto; 
+				
+				printf("Valor do Imposto: R$", imposto);
+				printf("Salário após o imposto: R$", salarioImposto);
 			
 		case 2:
+			printf("Digite o salário do funcionário: ");
+			scanf("%.2f", &salario);
 			
+			if (salario > 1500)
+				novoSalario = salario + 25;
+				printf("Novo Salário: R$", novoSalario);
+				
+			else if (salario >= 750 && salario <= 1500)
+				novoSalario = salario + 50;
+				printf("Novo Salário: R$", novoSalario);
+				
+			else if (salario >= 450 && salario <= 749)
+				novoSalario = salario + 75;
+				printf("Novo Salário: R$", novoSalario);
+				
+			else
+				novoSalario = salario + 100;
+				printf("Novo Salário: R$", novoSalario);
 			
 		case 3:
+			if (salario <= 700)
+				printf("Sua classificação é Mal Remunerado!");
+			else
+				printf("Sua classificação é Bem Remunerado!");
+				
+		default:
+			printf("OPÇÃO INVÁLIDA!!!");
 	}
+}
 	
 	
